60534328178dd45b6b0e2abe4edc62e9b4a10366
3podi <riccardo1.tripodi@mail.polimi.it>
2024-06-18 16:24:28 +0200

    Working dataloader with .h5

--- src/datasets/imagenet1k.py
+++ src/datasets/imagenet1k.py
@@ -44,9 +44,7 @@ def make_imagenet1k(
     data_loader = torch.utils.data.DataLoader(
         dataset,
         collate_fn=collator,
-        sampler=dist_sampler,
         batch_size=batch_size,
-        drop_last=drop_last,
         pin_memory=pin_mem,
         num_workers=num_workers,
         persistent_workers=False)
@@ -57,16 +55,17 @@ def make_imagenet1k(
 class GsocDataset(torch.utils.data.Dataset):
     def __init__(self, root, transform=None):
         self.h5_file = h5py.File(root, "r")
+        self.data = self.h5_file["jet"]
         self.transform = transform
 
     def __getitem__(self, index):
         if self.transform:
-            return self.transform(self.h5_file["jet"][index])
+           return self.data[index]
         else:
-            return self.h5_file["jet"][index]
+           return self.data[index]
         
     def __len__(self):
-        return self.h5_file["jet"].size
+        return self.data.shape[0]
 
 class CustomDataLoader():
     def __init__(self, file_path, indices, batch_size):
@@ -112,6 +111,13 @@ class CustomDataLoader():
         return data
 
     def process_data(self, data):
+
+        return data / 255.
+
+    def on_epoch_end(self):
+        # Shuffle indices at the end of each epoch
+        np.random.shuffle(self.indices)
+
 
         return data / 255.
 
