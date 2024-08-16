3e3fa92e0fbeb770c0cd10a1d97079c9d0ec11ba
riccardo <riccardo1.tripodi@mail.polimi.it>
2024-08-16 10:11:34 -0700

    iris commit
    added launch script for iris

---
+++ launch1.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#SBATCH -A m4392
+
+#SBATCH -C gpu
+
+#SBATCH -q preempt
+
+#SBATCH -t 24:00:00
+
+#SBATCH -n 1
+
+#SBATCH --ntasks-per-node=1
+
+#SBATCH -c 128
+
+#SBATCH --mem=0
+
+#SBATCH --gpus-per-task=1
+
+#SBATCH --requeue 
+
+start=
+(
+(
+(((date +%H)+
+(
+(
+(((date +%j)*24))))
+
+timeout 23h python3 main.py --fname configs/vit_b_14.yaml --devices cuda:0
+
+wait
+
+end=
+(
+(
+(((date +%H)+
+(
+(
+(((date +%j)*24))))
+
+tot=$((end-start))
+
+if [ $tot -gt 1 ]; then
+    sbatch ./Scripts/{script_name}
