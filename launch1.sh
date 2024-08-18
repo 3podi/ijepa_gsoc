bbeeb8ab4e7df32b2deb5c0a33b0f05eef4d71ca
Riccardo Tripodi <tripodi@login03.chn.perlmutter.nersc.gov>
2024-08-18 08:27:04 -0700

    3podi
    back to wandb, less than 24h training

--- launch1.sh
+++ launch1.sh
@@ -16,33 +16,12 @@
 
 #SBATCH --mem=0
 
-#SBATCH --gpus-per-task=3
+#SBATCH --gpus-per-task=2
 
 #SBATCH --requeue 
 
-start=
-(
-(
-(((date +%H)+
-(
-(
-(((date +%j)*24))))
 
 timeout 23h python3 main_iris.py --fname configs/vit_b_14_25_iris.yaml --devices cuda:0 &
-timeout 23h python3 main_iris.py --fname configs/vit_b_14_50_iris.yaml --devices cuda:0 &
-timeout 23h python3 main_iris.py --fname configs/vit_b_14_75_iris.yaml --devices cuda:0 &
+timeout 23h python3 main_iris.py --fname configs/vit_b_14_50_iris.yaml --devices cuda:1 &
 
 wait
-
-end=
-(
-(
-(((date +%H)+
-(
-(
-(((date +%j)*24))))
-
-tot=$((end-start))
-
-if [ $tot -gt 1 ]; then
-    sbatch ./Scripts/{script_name}
