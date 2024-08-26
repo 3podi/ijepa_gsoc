e5b7a7f90e159f287da327b5ec182e67d671d1d3
3podi <riccardo1.tripodi@mail.polimi.it>
2024-08-26 10:54:35 -0700

    3podi
    added launches

---
+++ launch_b_25_2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH -A m4392
+
+#SBATCH -C gpu
+
+#SBATCH -q preempt
+
+#SBATCH -t 24:00:00
+
+#SBATCH -n 1
+
+#SBATCH --ntasks-per-node=1
+
+#SBATCH -c 128
+
+#SBATCH --mem=0
+
+#SBATCH --gpus-per-task=1
+
+#SBATCH --requeue 
+
+
+python3 main_iris.py --fname configs/vit_b_7_25_iris.yaml --devices cuda:0
---
+++ launch_b_50.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH -A m4392
+
+#SBATCH -C gpu
+
+#SBATCH -q preempt
+
+#SBATCH -t 24:00:00
+
+#SBATCH -n 1
+
+#SBATCH --ntasks-per-node=1
+
+#SBATCH -c 128
+
+#SBATCH --mem=0
+
+#SBATCH --gpus-per-task=2
+
+#SBATCH --requeue 
+
+
+python3 main_iris.py --fname configs/vit_b_7_50_iris.yaml --devices cuda:0 &
+python3 main_iris.py --fname configs/vit_b_9_50_iris.yaml --devices cuda:1
